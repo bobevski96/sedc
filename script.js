f5423fc412401b8cc31401bc0af7ae647c20c14f
bobevski96 <106536988+bobevski96@users.noreply.github.com>
2022-12-06 17:44:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var phoneNumber = 30;
+var phonePrice = 119.95;
+
+var tax ;
+var totalSum ;
+
+
+tax = phonePrice * 0.05
+totalSum = phoneNumber * (phonePrice+tax);
+console.log(`For ${phoneNumber} phones, you have to play ${totalSum}`);
